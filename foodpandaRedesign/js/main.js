0cebb39802c53475853899f4239788fdf8869695
noman523 <37392852+noman523@users.noreply.github.com>
2021-09-10 18:04:32 +0600

    1st commit

---
+++ foodpandaRedesign/js/main.js
@@ -0,0 +1,37 @@
+/*---------------------------------------------
+Template name:  Clean
+Version:        1.0
+
+[Table of Content]
+
+01: Preloader
+02: Isotope
+03: MagnificPopup
+04: testimonial-slider
+05: client-logo-slider
+06: Counter up
+07: Back to Top
+08: back to top button animate
+----------------------------------------------*/
+
+(function($) {
+    "use strict";
+
+
+    var win = $(window);
+
+    /* ======= Preloader ======= */
+    win.on('load', function() {
+        $('#loading-area').delay('500').fadeOut(2000);
+    });
+
+    /* ======= Slick Slider ======== */
+
+
+
+
+
+
+
+
+})(jQuery);
